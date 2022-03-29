744c468abb36be40b1427748c1a5a804803aaba6
ffdy <1836017424@qq.com>
2022-03-29 18:12:27 +0800

    image update

---
+++ H3/app/src/main/java/cn/edu/swu/h3/MainActivity.kt
@@ -0,0 +1,19 @@
+package cn.edu.swu.h3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun showLife() {
+        TODO("Not yet implemented")
+    }
+
+    fun showWork() {
+        TODO("Not yet implemented")
+    }
+}
